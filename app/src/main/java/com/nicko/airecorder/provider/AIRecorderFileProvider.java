553335467ff2ae072a19a16b67bc9641dad2e93e
Nik <lookes777@yandex.ru>
2026-08-25 22:26:44 +0300

    Use dedicated FileProvider implementation

---
+++ app/src/main/java/com/nicko/airecorder/provider/AIRecorderFileProvider.java
@@ -0,0 +1,10 @@
+package com.nicko.airecorder.provider;
+
+import androidx.core.content.FileProvider;
+
+public class AIRecorderFileProvider extends FileProvider {
+
+    public AIRecorderFileProvider() {
+        super();
+    }
+}
